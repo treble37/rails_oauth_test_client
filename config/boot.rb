8cf1e22e443c077f59087b7e4a6c3479fed4dbab
Bruce <brucepark0@gmail.com>
2017-03-26 17:07:32 -0700

    Use port 4000

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,13 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require 'rails/commands/server'
+
+module DefaultOptions
+  def default_options
+    super.merge!(Port: 4000)
+  end
+end
+
+Rails::Server.send(:prepend, DefaultOptions)
